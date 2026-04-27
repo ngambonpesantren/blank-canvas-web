823eee494943fdde471f8a46392d588e5731a9bc
ngambonpesantren <ngambonpesantren@gmail.com>
2026-04-27 14:42:10 +0700

    clean-01

--- src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-const Index = () => {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-background">
-      <h1 className="text-foreground text-base font-normal">minimal</h1>
-    </div>
-  );
-};
-
-export default Index;
